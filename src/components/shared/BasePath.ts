2bcf94ad9bbb55f78aca3d6d75262285b7f42b4d
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2023-08-17 22:09:11 +0500

    basepath

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,5 +1,5 @@
 const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ?
     "http://localhost:3000"
-    : "https://dinemarket-b2o6ogfe5-bpspacer.vercel.app"
+    : "https://dinemarket-nhqxaydyr-bpspacer.vercel.app"
 
 export default BASE_PATH_FORAPI
