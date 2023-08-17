042f6ca5a46ced334a5aa57ce8621793a9cd3b00
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2023-08-17 20:44:51 +0500

    fix

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@ import CartComp from "@/components/views/CartParent/CartChild";
 import ContextWrapper from "@/global/context"
 
 async function fatchAllStoreProducts() {
-  let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-08-13/data/query/production?query=*[_type == 'products']`, {
+  let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-08-13/data/query/production?query=*%5B_type+%3D%3D+%22products%22%5D`, {
     cache: "no-store",
   })
   return res.json();
--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -29,7 +29,7 @@ async function fetchPreviewData(slug: string) {
 
 // will make static pages of every product
 export async function generateStaticParams() {
-    let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-08-13/data/query/production?query=*[_type == 'products']`, {
+    let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-08-13/data/query/production?query=*%5B_type+%3D%3D+%22products%22%5D`, {
         next: {
             revalidate: 60
         }
